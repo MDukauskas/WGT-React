a954bd6b21d21f5ceda8471e0c68f42d56e52ca6
Ieva Sinkeviciute <ieva1919@gmail.com>
2020-02-25 22:56:44 +0200

    added more style in the NewUserPage

--- src/pages/NewUserPage/index.js
+++ src/pages/NewUserPage/index.js
@@ -1,12 +1,40 @@
 import React, { useState } from 'react'
-import { InputGroup, Card, Button } from '../../components'
+import { InputGroup, Card, Button, Menu, Tabs } from '../../components'
 import './index.scss'
+
+import save from '../../save.svg';
+import newuser from '../../newuser.svg';
+
+const tabs = [{
+    title: 'General information',
+    content: <InputGroup />
+}, {
+    title: 'Comments',
+    content: 'Content 2'
+}]
 
 export const NewUserPage = () => {
 
     return (
-        <div>
-            <h1>On Boarding</h1>
+        <div className="newuser">
+            <Menu />
+            < div className="newuser-menu">
+                <h2>New User</h2>
+                <Tabs tabs={tabs} />
+                <div className="newuser-body">
+                    <div className="newuser-conten1">
+                        <InputGroup label="Name" type="text" />
+                        <InputGroup label="Surname" type="text" />
+                        <InputGroup label="Position" type="select" />
+                        <InputGroup label="Department" type="select" />
+                        <Button primary> <img src={save} alt="save" /> Button</Button>
+                        <Button> Button</Button>
+                    </div>
+                    <div className="newuser-conten2">
+                        <img src={newuser} alt="newuser" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
